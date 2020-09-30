5546c5caf10275d7f42d28a5e226e52152c4f5e6
RoPalma95 <r.palma995@gmail.com>
2020-09-29 23:03:36 -0600

    ETL solution

--- etl/etl.js
+++ etl/etl.js
@@ -1,13 +1,23 @@
-const oldSystem = {
-	1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-	2: ["D", "G"],
-	3: ["B", "C", "M", "P"],
-	4: ["F", "H", "V", "W", "Y"],
-	5: ["K"],
-	8: ["J", "X"],
-	10: ["Q", "Z"]
-}
+const oldSystem = new Map([
+	[1, ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]],
+	[2, ["D", "G"]],
+	[3, ["B", "C", "M", "P"]],
+	[4, ["F", "H", "V", "W", "Y"]],
+	[5, ["K"]],
+	[8, ["J", "X"]],
+	[10, ["Q", "Z"]]
+]);
 
-const transform = () => {
-	
+export const transform = (old) => {
+
+	let newSystem = new Object();
+
+	for (const points of oldSystem){  //points = [key, value]
+		if(old[points[0]]){
+			for(let i = 0; i < old[points[0]].length; i++){
+				newSystem[old[points[0]][i].toLowerCase()] = points[0];
+			}
+		}
+	}
+	return newSystem;
 };
